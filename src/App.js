936e3260d9af01fba1f8ce8f20028ec7138b9e7f
chathu1996 <galahitiyawa@gmail.com>
2021-11-10 11:51:14 +0530

    state concept

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
-  const myName = "Chathu"; 
+  let myName = "Chathu"; 
+  // hook
+  const [myName2, setMyName2] =  useState("Wije ");
+  const [num, setNum] = useState(18);
 
   function handleCLick() {
-    alert(myName);
+     myName = "Perera";
+    //  myName2 = "Perera";
+     setMyName2("Perera");
+  }
+  function increase(){
+    setNum(num + 1);
   }
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button 
-        onClick={handleCLick}
-      >
-        Click Me
-      </button>
+      <button onClick={ handleCLick }>Click Me</button>
+      <button onClick={ increase }>Increase Number</button>
       <br />
       {myName}
+      <br />
+      { myName2 }
+      <br />
+      Number : {num}
     </div>
   );
 }
