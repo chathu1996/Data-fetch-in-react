e431df6801a669138e3faef50506af82d7699da0
chathu1996 <galahitiyawa@gmail.com>
2021-11-10 12:39:35 +0530

    map function in array

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import { useState } from 'react';
-import './App.css';
+  import './App.css';
 
 
 function App() {
-  let myName = "Chathu"; 
-  // hook
-  const [myName2, setMyName2] =  useState("Wije ");
-  const [num, setNum] = useState(18);
-
-  function handleCLick() {
-     myName = "Perera";
-    //  myName2 = "Perera";
-     setMyName2("Perera");
-  }
-  function increase(){
-    setNum(num + 1);
-  }
+  // arrays of objects
+  const myArray = [
+    {name: "Silva", age: 32},
+    {name: "Kamal", age: 30},
+    {name: "Perera", age: 18},
+    {name: "Chamara", age: 21},
+  ];
 
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button onClick={ handleCLick }>Click Me</button>
-      <button onClick={ increase }>Increase Number</button>
-      <br />
-      {myName}
-      <br />
-      { myName2 }
-      <br />
-      Number : {num}
+      { myArray.map(function mapFunction(item) {
+           return <h1>{ item.name }, { item.age }</h1>;
+      })}
     </div>
   );
 }
